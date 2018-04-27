8f1de596c9f9289941988d50dda4787a50c845cd
Elad Cohen <eladc.android@gmail.com>
2018-04-27 10:23:35 +0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <div v-if="showNav" id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+    <a class="button is-primary" @click="toggleNav()">Primary</a>
     <router-view/>
   </div>
 </template>
+
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator';
+
+  @Component
+  export default class App extends Vue {
+    public showNav: boolean = true;
+    public toggleNav() {
+      this.showNav = !this.showNav;
+    }
+  }
+</script>
 
 <style lang="scss">
 #app {
